05e9817a2bac19df6e18354d086f600584d95b61
Viacheslav Greshilov <slovaricheg@gmail.com>
2022-07-23 18:24:09 +0300

    add monitoring

---
+++ monitor/monitor.py
@@ -0,0 +1,61 @@
+import datetime
+import contextlib
+import logging
+import os
+import sqlite3
+import time
+
+logger = logging.getLogger(__name__)
+DB = os.getenv("WG_MONITOR_DB", "./db.sqlite3")
+
+
+@contextlib.contextmanager
+def get_cursor():
+    con = sqlite3.connect(DB)
+
+    try:
+        cursor = con.cursor()
+        yield cursor
+    finally:
+        cursor.close()
+
+
+def create_monitor_tables():
+    logger.info("Creating monitoring table if not exists")
+    with get_cursor() as cursor:
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS "monitor" (
+            "owner" varchar(100),
+            "is_connected" boolean,
+            "receive_bytes" bigint,
+            "transmit_bytes" bigint,
+            "ts" datetime,
+        )""")
+
+
+def make_measure():
+    logger.info("Making a measure")
+
+    with get_cursor() as cursor:
+        cursor.execute("""
+        INSERT INTO "monitor" (
+            "owner",
+            "is_connected",
+            "receive_bytes",
+            "transmit_bytes",
+            "ts"
+        )
+        SELECT
+            "owner",
+            "last_handshake_time" > datetime(CURRENT_TIMESTAMP, '-5 minutes'),
+            "receive_bytes",
+            "transmit_bytes",
+            CURRENT_TIMESTAMP
+        FROM "devices"
+        """)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    create_monitor_tables()
+    make_measure()
